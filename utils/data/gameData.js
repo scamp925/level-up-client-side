78c24245b85763865cd83855615bff49837e50eb
Sariah Campopiano <scampopiano925@gmail.com>
2022-11-22 18:50:08 -0600

    Added fetch api calls to gameData.js for creating a game and get gametypes

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -7,5 +7,24 @@ const getGames = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-// eslint-disable-next-line import/prefer-default-export
-export { getGames };
+const createGame = (game) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/games`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(game),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getGameTypes = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/gametype`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export { getGames, createGame, getGameTypes };
