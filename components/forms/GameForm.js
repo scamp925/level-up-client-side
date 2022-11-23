e581ca741ab97e0fcb00a99d8e4fdfb2109ae0ad
Sariah Campopiano <scampopiano925@gmail.com>
2022-11-22 19:11:38 -0600

    Added the start of a game form, pages/new.js and added a button to the games page to add a game

---
+++ components/forms/GameForm.js
@@ -0,0 +1,71 @@
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { createGame, getGameTypes } from '../../utils/data/gameData';
+
+const GameForm = ({ user }) => {
+  const [gameTypes, setGameTypes] = useState([]);
+  /*
+  Since the input fields are bound to the values of
+  the properties of this state variable, you need to
+  provide some default values.
+  */
+  const [currentGame, setCurrentGame] = useState({
+    skillLevel: 1,
+    numberOfPlayers: 0,
+    title: '',
+    maker: '',
+    gameTypeId: 0,
+  });
+  const router = useRouter();
+
+  useEffect(() => {
+    // TODO: Get the game types, then set the state
+  }, []);
+
+  const handleChange = (e) => {
+    // TODO: Complete the onChange function
+  };
+
+  const handleSubmit = (e) => {
+    // Prevent form from being submitted
+    e.preventDefault();
+
+    const game = {
+      maker: currentGame.maker,
+      title: currentGame.title,
+      number_of_players: Number(currentGame.numberOfPlayers),
+      skill_level: Number(currentGame.skillLevel),
+      game_type: Number(currentGame.gameTypeId),
+      user_id: user.uid,
+    };
+
+    // Send POST request to your API
+    createGame(game).then(() => router.push('/games'));
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control name="title" required value={currentGame.title} onChange={handleChange} />
+        </Form.Group>
+        {/* TODO: create the rest of the input fields */}
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+GameForm.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default GameForm;
