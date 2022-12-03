2fdb90036a7ee43fac05d99fa81c7c7c2135e610
Sariah Campopiano <scampopiano925@gmail.com>
2022-12-03 10:12:58 -0600

    Added onUpdate to GameCard and games/index.js. Able to delete game and once a game is deleted any assoicated event with that game is also deleted

--- components/cards/GameCard.js
+++ components/cards/GameCard.js
@@ -1,27 +1,42 @@
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { deleteGame } from '../../utils/data/gameData';
 
-const GameCard = ({
+function GameCard({
   title, //
   maker,
   numberOfPlayers,
   skillLevel,
   id,
-}) => (
-  <Card className="text-center">
-    <Card.Body>
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>By: {maker}</Card.Text>
-      <Card.Text>Skill Level: {skillLevel}</Card.Text>
-      <Card.Text>{numberOfPlayers} players needed</Card.Text>
-    </Card.Body>
-    <Card.Footer className="text-muted">
-      <Button variant="link" href={`/games/edit/${id}`}>Edit</Button>
-      <Button variant="link">Delete</Button>
-    </Card.Footer>
-  </Card>
-);
+  onUpdate,
+}) {
+  const router = useRouter();
+
+  const deleteThisGame = () => {
+    if (window.confirm(`Heads up! You are about to permanently delete ${title}. Click "OK" if you wish to continue.`)) {
+      deleteGame(id).then(() => onUpdate()).then(() => {
+        router.push('/games');
+      });
+    }
+  };
+
+  return (
+    <Card className="text-center">
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>By: {maker}</Card.Text>
+        <Card.Text>Skill Level: {skillLevel}</Card.Text>
+        <Card.Text>{numberOfPlayers} players needed</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">
+        <Button variant="link" href={`/games/edit/${id}`}>Edit</Button>
+        <Button variant="link" onClick={deleteThisGame}>Delete</Button>
+      </Card.Footer>
+    </Card>
+  );
+}
 
 GameCard.propTypes = {
   id: PropTypes.number.isRequired,
@@ -29,6 +44,7 @@ GameCard.propTypes = {
   maker: PropTypes.string.isRequired,
   numberOfPlayers: PropTypes.number.isRequired,
   skillLevel: PropTypes.number.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default GameCard;
