775e8611ee29571a0c256f19a5491f6ad7ef7671
Sariah Campopiano <scampopiano925@gmail.com>
2022-12-03 12:13:47 -0600

    Implemented the spread operator in both events/index and games/index when call the respective cards instead of typing out all of the keys that match in both the front-end and back-end

--- pages/events/index.js
+++ pages/events/index.js
@@ -30,7 +30,7 @@ export default function ViewAllEvents() {
       </Button>
       {events.map(((event) => (
         <section key={`event--${event.id}`} className="event">
-          <EventCard id={event.id} game={event.game} description={event.description} date={event.date} time={event.time} organizer={event.organizer} onUpdate={getAllEvents} />
+          <EventCard {...event} onUpdate={getAllEvents} />
         </section>
       )))}
     </article>
